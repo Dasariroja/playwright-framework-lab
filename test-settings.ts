c5e5256ff86fe57fa3bf710d1fe6ae2922b0115e
Team-testum <testers.testum@gmail.com>
2026-04-04 16:28:13 +0100

    Added env and test data files

---
+++ test-settings.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://www.saucedemo.com';
+export const API_BASE_URL = 'https://api.postcodes.io';
+export const INVENTORY_URL = `${BASE_URL}/inventory.html`;
+export const CHECKOUT_URL = `${BASE_URL}/cart.html`;
